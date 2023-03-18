af3d91773d18bdbe89dca6842061792b87108c1a
Renzo <81322658+RenzoMarangon@users.noreply.github.com>
2023-03-17 23:35:43 -0300

    Delete webAnimalesExoticos directory

--- webAnimalesExoticos/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$base="flix";
-$con=mysqli_connect("localhost","root","",$base);
-if($con){
-	//echo "conectado";
-}else{
-	echo mysql_error();
-}
-?>
--- webAnimalesExoticos/guardarimagen.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$carpeta="imagenes";
-if(copy($_IMAGEN['imagen']['tmp_name'],$carpeta."/".$_IMAGEN['imagen']['animal'])){
-	echo "<b>".$carpeta."/".$_IMAGEN['imagen']['animal']."<b>";
-}else{
-	echo "archivo no subido";
-}
-?>
